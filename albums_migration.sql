5587359340138bf2a06402ad06602d8d87b7cc78
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-03-17 17:31:18 -0500

    added rest of dates to years

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,13 +1,15 @@
-USE codeup_test_db;
+USE
+codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
 
-CREATE TABLE albums(
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  artist VARCHAR(50),
-  name VARCHAR(50) NOT NULL,
-  release_date DATE,
-    sales FLOAT,
-    genre VARCHAR(200),
+CREATE TABLE albums
+(
+    id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist       VARCHAR(50),
+    name         VARCHAR(50) NOT NULL,
+    release_date DATE,
+    genre        VARCHAR(200),
+    sales        FLOAT,
     PRIMARY KEY (id)
 );
